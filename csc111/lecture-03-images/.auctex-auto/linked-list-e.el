7a1520c4b822a8fc4fae9dfda29edc8264a55a52
hisbaan <34548959+hisbaan@users.noreply.github.com>
2021-03-31 11:27:23 -0400

    Add notes for CSC110 and CSC111

---
+++ csc111/lecture-03-images/.auctex-auto/linked-list-e.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "linked-list-e"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("standalone" "border=0.2cm")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=1in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "standalone"
+    "standalone10"
+    "geometry"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "graphicx"
+    "multicol"
+    "array"
+    "tikz"))
+ :latex)
+
