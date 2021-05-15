5f9afd0b1dc4f44316a2d6cdedb63243e224aaac
hisbaan <34548959+hisbaan@users.noreply.github.com>
2021-05-15 16:44:52 -0400

    Update structure of repo to allow for more courses

--- update-full.sh
+++ update-full.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-rm $DIR/csc110.pdf $DIR/csc111.pdf
-pdftk $DIR/csc110/*.pdf cat output $DIR/csc110.pdf
-pdftk $DIR/csc111/*.pdf cat output $DIR/csc111.pdf
+rm "$DIR"/csc110.pdf "$DIR"/csc111.pdf
+pdftk "$DIR"/csc110/src/*.pdf cat output "$DIR"/csc110/csc110.pdf
+pdftk "$DIR"/csc111/src/*.pdf cat output "$DIR"/csc111/csc111.pdf
