488e47798a392668ad332e1c5a5b671c8c0d3c7a
hisbaan <34548959+hisbaan@users.noreply.github.com>
2021-08-21 22:26:04 -0400

    Update readme and add base files for MAT137 notes

--- update-full.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-rm "$DIR"/csc110.pdf "$DIR"/csc111.pdf
-pdftk "$DIR"/csc110/src/*.pdf cat output "$DIR"/csc110/csc110.pdf
-pdftk "$DIR"/csc111/src/*.pdf cat output "$DIR"/csc111/csc111.pdf
